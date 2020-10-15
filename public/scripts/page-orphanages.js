cbe08873eb3018e80f02703c62b542155a0d9b3c
Marina Rozas <rozasmarina@gmail.com>
2020-10-15 13:05:16 -0300

    styling map

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -5,8 +5,25 @@ const map = L.map('mapid').setView([-23.6821604, -46.8754915], 15)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 
 //create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+
+//create popup overlay
+
+const popup = L.popup({
+  closeButton: false,
+  classname: 'map-popup',
+  minWidth: 240,
+  minHeight: 240
+}).setContent('Lar das Meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+//create and add marker
 L
-  .marker([-23.6821604, -46.8754915])
+  .marker([-23.6821604, -46.8754915], {
+    icon
+  })
   .addTo(map)
-  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  .openPopup();
+  .bindPopup(popup)
